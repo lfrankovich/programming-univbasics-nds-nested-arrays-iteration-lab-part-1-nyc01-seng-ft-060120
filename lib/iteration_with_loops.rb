02d020c307f115be77c2b6c8c387c63691100979
Lauren Frankovich <frankovister@gmail.com>
2020-05-26 00:00:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,8 @@ row_index = 0
 while row_index < find_even_values.count do
    element_index = 0
    while element_index < find_even_values[row_index].count do
-      p spice_rack[row_index][element_index]
+     if element_index % 2 == 0
+      p find_even_values[row_index][element_index]
       element_index += 1
    end
    row_index += 1
